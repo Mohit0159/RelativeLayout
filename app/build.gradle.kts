fe7a01bcb125cb0fa88a89f164a6210494264c24
Mohit0159 <164319184+Mohit0159@users.noreply.github.com>
2025-01-13 22:36:21 +0530

    ui design and intent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.relativelayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.relativelayout"
--- app/src/main/java/com/example/relativelayout/Screen2.kt
+++ app/src/main/java/com/example/relativelayout/Screen2.kt
@@ -1,33 +1,20 @@
 package com.example.relativelayout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    var btn: Button?=null
-    var btnLinear: Button? = null
-    var input: EditText? = null
+class Screen2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_screen2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        btn = findViewById(R.id.bt1)
-        btn?.setOnClickListener {
-            var intent= Intent(this,Screen2::class.java)
-            startActivity(intent)
-            Toast.makeText(this,"Sign in Successfully", Toast.LENGTH_SHORT).show()
         }
     }
 }
